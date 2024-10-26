183e3587c7dfd8844bd188796a652800cc54774c
Broc Keeton <broc.keeton@student.centralia.edu>
2024-10-26 13:45:12 -0700

    Add project files.

---
+++ RecursiveSumOfArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = { 1, 2, 3, 4, 5 };
+int sum = ArraySumLab.SumArray(numbers, numbers.Length);
+Console.WriteLine($"Sum of array: {sum}");
+
+public class ArraySumLab
+{
+    static public int SumArray(int[] array, int n)
+    {
+       if (n <= 0)
+       {
+           return 0;
+       } 
+       else
+       {
+           return (SumArray(array, n - 1) + array[n - 1]);
+       }
+    }
+}
+
